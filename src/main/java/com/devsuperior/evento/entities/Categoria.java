076fbd90b3095523c09431b10beedb45a66ba17b
primoalexandre <primoalexandre069@gmail.com>
2023-08-11 14:25:10 -0300

    p

---
+++ src/main/java/com/devsuperior/evento/entities/Categoria.java
@@ -0,0 +1,57 @@
+package com.devsuperior.evento.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(columnDefinition="TEXT")
+	private String descricao;
+	
+	@OneToMany(mappedBy="categoria")
+	private List<Atividade> atividades = new ArrayList<>();
+	
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String descricao) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Atividade> getAtividades() {
+		return atividades;
+	}
+
+	
+}
